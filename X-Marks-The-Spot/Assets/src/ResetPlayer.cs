39bd3bd616e6697be48db2e782aedfd630a88b66
axelas-4 <axelas-4@student.ltu.se>
2015-11-10 15:25:55 +0100

    Added Corner
    ResetPlayer not done

---
+++ X-Marks-The-Spot/Assets/src/ResetPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetPlayer : MonoBehaviour
+{
+    private Vector3 StartPosition;
+    public Transform PlayerTransform;
+	
+	void Start ()
+    {
+        StartPosition = PlayerTransform.position;
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            PlayerTransform.position.Set(StartPosition[0], StartPosition[1], StartPosition[2]);
+        }
+    }
+}
