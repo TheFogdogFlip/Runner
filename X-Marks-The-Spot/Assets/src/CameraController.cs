e4a73f8b4411b3818a0dedf9f340ee83748ff263
swerik95 <erkhed-4@student.ltu.se>
2015-11-10 16:24:44 +0100

    Progression on the player camera.

--- X-Marks-The-Spot/Assets/src/CameraController.cs
+++ X-Marks-The-Spot/Assets/src/CameraController.cs
@@ -6,8 +6,18 @@ public class CameraController : MonoBehaviour {
     public GameObject Player;
     private Vector3 distance;
     private Vector3 holder;
+    private Vector3 startpos;
+    private Vector3 axis;
+    private float angle;
 	// Use this for initialization
 	void Start () {
+        angle = 1;
+        startpos[1] = -3;
+        startpos[2] = 10;
+        axis[0] = 0;
+        axis[1] = 1;
+        axis[2] = 0;
+        transform.position = Player.transform.position - startpos;
         distance = transform.position - Player.transform.position;
 	}
 	
@@ -16,6 +26,6 @@ public class CameraController : MonoBehaviour {
         holder.z = Player.transform.position.z;
         holder.x = Player.transform.position.x;
         transform.position = holder + distance;
-        transform.Rotate(Player.transform.up * Time.deltaTime);
+        transform.RotateAround(Player.transform.position, axis, angle);
     }
 }
