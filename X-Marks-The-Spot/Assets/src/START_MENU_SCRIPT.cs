8e9dc0a488e029f79d0f573efab0ffe8398db6bc
axelas-4 <axelas-4@student.ltu.se>
2015-11-10 15:49:57 +0100

    Revert "loads!"
    This reverts commit 47552db5d88c703c041bb8f230440eb0e0c63b4a.

--- X-Marks-The-Spot/Assets/src/START_MENU_SCRIPT.cs
+++ X-Marks-The-Spot/Assets/src/START_MENU_SCRIPT.cs
@@ -6,7 +6,6 @@ public class START_MENU_SCRIPT : MonoBehaviour
 {
     public Canvas quitMenu;
     public Button startText;
-    public Button loadLevelText;
     public Button exitText;
     public Button optionsText;
 
@@ -16,14 +15,12 @@ public class START_MENU_SCRIPT : MonoBehaviour
         startText = startText.GetComponent<Button>();
         exitText = exitText.GetComponent<Button>();
         optionsText = optionsText.GetComponent<Button>();
-        loadLevelText = loadLevelText.GetComponent<Button>();
         quitMenu.enabled = false;
 	}
 	
 	public void ExitPress()
     {
         quitMenu.enabled = true;
-        loadLevelText.enabled = false;
         startText.enabled = false;
         exitText.enabled = false;
         optionsText.enabled = false;
@@ -32,7 +29,6 @@ public class START_MENU_SCRIPT : MonoBehaviour
     public void NoPress()
     {
         quitMenu.enabled = false;
-        loadLevelText.enabled = true;
         startText.enabled = true;
         exitText.enabled = true;
         optionsText.enabled = true;
@@ -40,13 +36,9 @@ public class START_MENU_SCRIPT : MonoBehaviour
 
     public void StartLevel()
     {
-        Application.LoadLevel(3);
-    }
-
-    public void LoadLevelPress()
-    {
         Application.LoadLevel(2);
     }
+
     public void OptionsPress()
     {
         Application.LoadLevel(1);
