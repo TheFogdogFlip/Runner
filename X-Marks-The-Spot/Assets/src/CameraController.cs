3706fa27be339fc1d3a0c55630c83fe6591ae9cd
swerik95 <erkhed-4@student.ltu.se>
2015-11-16 10:49:35 +0100

    PlayerCamera tweeks

--- X-Marks-The-Spot/Assets/src/CameraController.cs
+++ X-Marks-The-Spot/Assets/src/CameraController.cs
@@ -4,28 +4,40 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
     public GameObject Player;
-    private Vector3 distance;
-    private Vector3 holder;
-    private Vector3 startpos;
-    private Vector3 axis;
-    private float angle;
+    private float distance;
+    private float height;
+    private float heightDamping;
+    private float rotationDamping;
+    private float rotAngle;
+    private float currentRotAngle;
+    private Quaternion currentRot;
+    private float wantedHeight;
+    private float currentHeight;
+    private Vector3 heightVec;
 	// Use this for initialization
 	void Start () {
-        angle = 1;
-        startpos[1] = -3;
-        startpos[2] = 10;
-        axis[0] = 0;
-        axis[1] = 1;
-        axis[2] = 0;
-        transform.position = Player.transform.position - startpos;
-        distance = transform.position - Player.transform.position;
-	}
+        distance = 3.0f;
+        height = 0.1f;
+        heightDamping = 3.0f;
+        rotationDamping = 5.0f;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        holder.z = Player.transform.position.z;
-        holder.x = Player.transform.position.x;
-        transform.position = holder + distance;
-        transform.RotateAround(Player.transform.position, axis, angle);
+        rotAngle = Player.transform.eulerAngles.y;
+        wantedHeight = Player.transform.position.y + height;
+        //smooth rot
+        currentRotAngle = Mathf.LerpAngle(currentRotAngle, rotAngle, rotationDamping * Time.deltaTime);
+        currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+        currentRot = Quaternion.Euler(0, currentRotAngle, 0);
+
+        heightVec[1] = currentHeight;
+        transform.position = Player.transform.position + heightVec;
+        transform.position -= currentRot * Vector3.forward * distance;
+
+        //transform.position.y = currentHeight;
+
+        transform.LookAt(Player.transform);
     }
 }
