d2191af93606286a13b71c60cbd7620d66f33b38
Zackarias A <th3_z@hotmail.com>
2015-11-17 16:07:50 +0100

    Removed some unnecessary debugs

--- X-Marks-The-Spot/Assets/src/Player.cs
+++ X-Marks-The-Spot/Assets/src/Player.cs
@@ -59,7 +59,6 @@ public class Player : MonoBehaviour
         bc = GetComponent<BoxCollider>();
         crntSlideLength = maxSlideLength;
         rotationTarget = transform.rotation.y;
-        Debug.Log(World.GridDimentions);
         SetupCtdTimer();
         //SetupGhostTimer();
         SetupPlayerTimer();
@@ -315,7 +314,6 @@ public class Player : MonoBehaviour
             Ghost ghost;
             ghostinputs.Add(inputs);
             inputs = new List<TimeStamp>();
-            print(ghostinputs.Count);
             for (int i = 0; i < ghostinputs.Count; ++i)
             {
                 go = Instantiate(Resources.Load("Ghost", typeof(GameObject)), World.Instance.StartPosition, Quaternion.Euler(World.Instance.StartDirection)) as GameObject;
