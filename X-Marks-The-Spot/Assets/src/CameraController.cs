d4f2e2cce78a785f42afb9cc7f9746d793372bfc
swerik95 <erkhed-4@student.ltu.se>
2015-11-17 14:19:33 +0100

    PlayerCamFix

--- X-Marks-The-Spot/Assets/src/CameraController.cs
+++ X-Marks-The-Spot/Assets/src/CameraController.cs
@@ -14,22 +14,21 @@ public class CameraController : MonoBehaviour {
     private float wantedHeight;
     private float currentHeight;
     private Vector3 heightVec;
-    private Vector3 lookAtVec;
-    private Transform lookAtPos;
+    private Vector3 tiltVec;
 
     // Use this for initialization
     void Start () {
-        distance = 1.5f;
-        height = 0.5f;
+        distance = 1.2f;
+        height = 0.8f;
         heightDamping = 3.0f;
         rotationDamping = 5.0f;
         target = GameObject.Find("Player(Clone)").transform;
-        lookAtVec[1] = height;
+        tiltVec[1] = 0.4f;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        //target = GameObject.Find("Player(Clone)").transform;
+        //wanted rotation and height
         rotAngle = target.eulerAngles.y;
         wantedHeight = target.position.y + height;
 
@@ -39,13 +38,13 @@ public class CameraController : MonoBehaviour {
 
         currentRot = Quaternion.Euler(0, currentRotAngle, 0);
 
-
+        //camera transform
         this.gameObject.transform.position = target.position;
         this.gameObject.transform.position -= currentRot * Vector3.forward * distance;
         heightVec[1] = currentHeight;
         this.gameObject.transform.position = this.gameObject.transform.position + heightVec;
+
         //LookAt
-        //lookAtPos.position = target.position + lookAtVec;
-        this.gameObject.transform.LookAt(target);
+        this.gameObject.transform.LookAt(target.position + tiltVec);
     }
 }
