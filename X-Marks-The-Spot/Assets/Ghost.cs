53af07ed57394684256026a9337855f820923e97
steinwall <erik.steinvall@hotmail.se>
2015-11-17 12:39:22 +0100

    ghost with delta time

---
+++ X-Marks-The-Spot/Assets/Ghost.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+public class Ghost : MonoBehaviour {
+
+    //For moving forward
+    public float runSpeed;
+    public float crntSpeed;
+    public float deceleration;
+    public float acceleration;
+
+    //For turning 90 degrees smoothly
+    public float turnSpeed = 55.0f;
+    private float rotationTarget;
+    private Quaternion qTo = Quaternion.identity;
+    private int turnPhase = 0;
+
+    public List<TimeStamp> inputs;
+    private Timer_Ghost ghostTimerObj;
+    private GameObject ghostTimerGameObj;
+    int index;
+    // Use this for initialization
+    void Start () {
+        
+        index = 0;
+        rotationTarget = 0;
+        crntSpeed = runSpeed;
+        ghostTimerGameObj = GameObject.Find("ghostTimer");
+        ghostTimerObj = ghostTimerGameObj.GetComponent<Timer_Ghost>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ghostTimerObj.SetText();
+        Turn();
+        Run();
+
+    }
+    void Turn()
+    {
+        if(index < inputs.Count)
+        {
+            //print(inputs[index].time.ToString() + " " + ghostTimerObj.f_time.ToString());
+            if (inputs[index].time <= ghostTimerObj.f_time)
+            {
+                if (inputs[index].input == "TurnRight")
+                {
+
+                    rotationTarget += 90.0f;
+                    //anim.Play("TurnRight90");
+                    turnPhase = 1;
+
+                }
+                else if (inputs[index].input == "TurnLeft")
+                {
+
+                    rotationTarget -= 90.0f;
+                    //anim.Play("TurnLeft90");
+                    turnPhase = 1;
+                }
+                if (rotationTarget == 360 || rotationTarget == -360)
+                {
+                    rotationTarget = 0.0f;
+                }
+                qTo = Quaternion.Euler(0.0f, rotationTarget, 0.0f);
+                index++;
+            }
+            
+        }
+       
+
+        //BRAKING PHASE
+        if (turnPhase == 1)
+        {
+            crntSpeed -= deceleration;
+
+            if (crntSpeed <= 0.01)
+            {
+                turnPhase = 2;
+            }
+        }
+
+        //TURNING PHASE
+        if (turnPhase == 2)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, qTo, turnSpeed * Time.deltaTime);
+
+            if (transform.rotation == qTo)
+            {
+                turnPhase = 3;
+            }
+        }
+        //AXELERATION PHASE
+        if (turnPhase == 3)
+        {
+            crntSpeed += acceleration;
+            if (crntSpeed >= runSpeed)
+            {
+                crntSpeed = runSpeed;
+                turnPhase = 0;
+            }
+        }
+    }
+
+    void Run()
+    {
+        //Run forward
+        transform.Translate(transform.forward * crntSpeed * 100 * Time.deltaTime, Space.World);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- X-Marks-The-Spot/Assets/src/ResetPlayer.cs
+++ X-Marks-The-Spot/Assets/src/ResetPlayer.cs
@@ -1,8 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ResetPlayer : MonoBehaviour
 {
+    private Timer_Ghost ghostTimerObj;
+    private GameObject ghostTimerGameObj;
+
+    private Player player;
+    private List<List<TimeStamp>> ghostinputs;
+    private List<GameObject> ghosts;
+
     private Vector3 StartPosition;
     private Quaternion StartRotation;
     private Transform PlayerTransform;
@@ -12,6 +20,9 @@ public class ResetPlayer : MonoBehaviour
         PlayerTransform = GetComponent<Transform>();
         StartPosition = PlayerTransform.position;
         StartRotation = PlayerTransform.rotation;
+        player = GetComponent<Player>();
+        ghostinputs = new List<List<TimeStamp>>();
+        ghosts = new List<GameObject>();
 	}
 	
 	void Update ()
@@ -23,7 +34,15 @@ public class ResetPlayer : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Wall"))
         {
+            
             Death();
+            
+            GameObject go = Instantiate(Resources.Load("Ghost", typeof(GameObject)), StartPosition, StartRotation) as GameObject;
+            Ghost ghost = go.GetComponent<Ghost>();
+            ghost.inputs = player.inputs;
+            ghosts.Add(go);
+            ghostinputs.Add(player.inputs);
+            SetupGhostTimer();
         }
     }
 
@@ -31,5 +50,28 @@ public class ResetPlayer : MonoBehaviour
     {
         Destroy(gameObject);
         Instantiate(Resources.Load("Player", typeof(GameObject)), StartPosition, StartRotation);
+        foreach(GameObject go in ghosts)
+        {
+            Destroy(go);
+        }
+        for(int i = 0; i < ghostinputs.Count; ++i)
+        {
+            GameObject go = Instantiate(Resources.Load("Ghost", typeof(GameObject)), StartPosition, StartRotation) as GameObject;
+            Ghost ghost = go.GetComponent<Ghost>();
+            ghost.inputs = ghostinputs[i];
+            ghosts.Add(go);
+            
+        }
+
+    }
+    void SetupGhostTimer()
+    {
+        ghostTimerGameObj = GameObject.Find("ghostTimer");
+        ghostTimerObj = ghostTimerGameObj.GetComponent<Timer_Ghost>();
+        ghostTimerObj.f_time = 0;
+        ghostTimerObj.textObj.text = "0";
+        ghostTimerObj.TimerRunning = true;
+        
+
     }
 }
