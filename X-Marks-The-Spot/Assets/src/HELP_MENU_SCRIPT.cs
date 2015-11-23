2ef1902bdf8a6f16bc684cecde26c3197065c9d1
Evixy <calle_holst@msn.com>
2015-11-23 10:42:38 +0100

    menus reworked

--- X-Marks-The-Spot/Assets/src/HELP_MENU_SCRIPT.cs
+++ X-Marks-The-Spot/Assets/src/HELP_MENU_SCRIPT.cs
@@ -2,24 +2,24 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class LOAD_LEVEL_SCRIPT : MonoBehaviour 
-{
+public class HELP_MENU_SCRIPT : MonoBehaviour {
+
     public Canvas startMenu;
-    public Canvas loadLevelMenu;
+    public Canvas helpMenu;
 
     public Button backText;
 
-    // Use this for initialization
-    void Start()
+	// Use this for initialization
+	void Start() 
     {
         startMenu = startMenu.GetComponent<Canvas>();
-        loadLevelMenu = loadLevelMenu.GetComponent<Canvas>();
+        helpMenu = helpMenu.GetComponent<Canvas>();
         backText = backText.GetComponent<Button>();
-    }
-
+	}
+	
     public void BackPress()
     {
-        loadLevelMenu.enabled = false;
+        helpMenu.enabled = false;
         startMenu.enabled = true;
     }
 }
--- X-Marks-The-Spot/Assets/src/OPTIONS_MENU_SCRIPT.cs
+++ X-Marks-The-Spot/Assets/src/OPTIONS_MENU_SCRIPT.cs
@@ -4,16 +4,22 @@ using System.Collections;
 
 public class OPTIONS_MENU_SCRIPT : MonoBehaviour 
 {
+    public Canvas startMenu;
+    public Canvas optionsMenu;
+
     public Button backText;
 
 	// Use this for initialization
 	void Start() 
     {
+        startMenu = startMenu.GetComponent<Canvas>();
+        optionsMenu = optionsMenu.GetComponent<Canvas>();
         backText = backText.GetComponent<Button>();
 	}
-	
-	public void BackPress()
+
+    public void BackPress()
     {
-        Application.LoadLevel(0);
+        optionsMenu.enabled = false;
+        startMenu.enabled = true;
     }
 }
