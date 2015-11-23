7cfea03a16f52c15c0b8ed09567db26f3fdb24da
swerik95 <erkhed-4@student.ltu.se>
2015-11-23 10:57:13 +0100

    Added  test soundtrack.

--- X-Marks-The-Spot/Assets/src/CameraController.cs
+++ X-Marks-The-Spot/Assets/src/CameraController.cs
@@ -20,39 +20,42 @@ public class CameraController : MonoBehaviour {
     private Vector3 heightVec;
     private Vector3 tiltVec;
     private float tilt;
-
+    private Transform lookAtTrans;
+    private Vector3 lookAtVec;
     // Use this for initialization
     void Start () {
         distance = 1.2f;
         height = 0.7f;
-        heightDamping = 3.0f;
+        heightDamping = 5.0f;
         rotationDamping = 5.0f;
         target = GameObject.Find("Player(Clone)").transform;
-        //tiltVec[1] = 0.4f;
-        tilt = 0.3f;
+        tilt = 0.2f;
     }
 	
 	// Update is called once per frame
 	void Update () {
         //wanted rotation and height
         rotAngleY = target.eulerAngles.y;
-        rotAngleX = target.eulerAngles.x;
         wantedHeight = target.position.y*0f + height;
 
         //smooth rot
         currentRotAngleY = Mathf.LerpAngle(currentRotAngleY, rotAngleY, rotationDamping * Time.deltaTime);
-        currentRotAngleX = Mathf.LerpAngle(currentRotAngleX, rotAngleX, rotationDamping * Time.deltaTime);
         currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
 
-        currentRot = Quaternion.Euler(currentRotAngleX, currentRotAngleY, 0);
+        currentRot = Quaternion.Euler(0, currentRotAngleY, 0);
 
         //camera transform
         this.gameObject.transform.position = target.position;
-        this.gameObject.transform.position -= currentRot * Vector3.forward * distance;
-        heightVec[1] = currentHeight;
+        this.gameObject.transform.position -= (currentRot * Vector3.forward * distance);
+        heightVec[1] = height;
         this.gameObject.transform.position = this.gameObject.transform.position + heightVec;
         tiltVec[1] = currentHeight - tilt;
         //LookAt
+        //lookAtTrans = target;
+        //lookAtVec[0] = target.position.x;
+        //lookAtVec[2] = target.position.z;
+        //lookAtTrans.position = lookAtVec;
+        //lookAtTrans.position = new Vector3(target.position.x, 0, target.position.z);
         this.gameObject.transform.LookAt(target.position + tiltVec);
     }
 }
