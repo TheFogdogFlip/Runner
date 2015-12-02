7d323acc23903b5bf6fcf7545abfbb3a192b455f
Zackarias A <th3_z@hotmail.com>
2015-12-02 08:56:13 +0100

    Fixed nullreference error

--- X-Marks-The-Spot/Assets/src/UI_Arrows.cs
+++ X-Marks-The-Spot/Assets/src/UI_Arrows.cs
@@ -7,17 +7,25 @@ public class UI_Arrows : MonoBehaviour {
     RawImage riLeft;
     RawImage riRight;
     Player pl;
+    bool isFirstFrame;
 
 	void Start ()
     {
+        isFirstFrame = true;
         riLeft = GameObject.Find("UI_Left").GetComponent<RawImage>();
         riRight = GameObject.Find("UI_Right").GetComponent<RawImage>();
-        pl = GameObject.Find("Player(Clone)").GetComponent<Player>();
+        
 	}
 	
 	
 	void Update ()
     {
+        if (isFirstFrame)
+        {
+            pl = GameObject.Find("Player(Clone)").GetComponent<Player>();
+            isFirstFrame = false;
+        }
+
 	    if (pl.GetNextAction() == PlayerState.TurnLeft)
         {
             riRight.enabled = false;
