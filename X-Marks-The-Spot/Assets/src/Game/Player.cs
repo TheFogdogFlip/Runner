cb04d5ab9e2770e63ad81a42276d127a7f018f96
axelas-4 <axelas-4@student.ltu.se>
2015-12-15 09:47:50 +0100

    Animation bug solved.

--- X-Marks-The-Spot/Assets/src/Game/Player.cs
+++ X-Marks-The-Spot/Assets/src/Game/Player.cs
@@ -310,7 +310,6 @@ public class Player : PlayerBase
     Falling()
     {
         AudioManager.Instance.FallingSound();
-        anim.SetTrigger("Slide");
     }
 
     /**---------------------------------------------------------------------------------
